14fcf860dcad30e61eac7e1e3b9f582ff57253e5
Daktic <gberg@DakticDigital.com>
2024-05-30 14:49:59 -0500

    Added holders for nums

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -22,10 +22,7 @@ function App() {
     }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
 
-      </header>
       <LandingPage />
         <button onClick={maKeRequest}>Click me</button>
 
--- frontend/src/landing/landingPage.js
+++ frontend/src/landing/landingPage.js
@@ -3,7 +3,47 @@
 function LandingPage() {
   return (
     <div>
-      <h1>Landing Page</h1>
+      <h1>Pool Metric Aggregator</h1>
+        <div>
+            <div>
+                <h3>Total Supply</h3>
+                <p>0</p>
+            </div>
+            <div>
+                <h3>Locked in fee Pool</h3>
+                <p>1</p>
+            </div>
+            <div>
+                <h3>Reserved Amount</h3>
+                <p>2</p>
+            </div>
+            <div>
+                <h3>Circulating Supply</h3>
+                <p>3</p>
+            </div>
+            <div>
+                <h3>Trustlessness</h3>
+                <p>4</p>
+            </div>
+            <div>
+                <h3>Total Payments count</h3>
+                <p>5</p>
+            </div>
+            <div>
+                <h3>Overall Payment Count</h3>
+                <p>6</p>
+            </div>
+            <div>
+                <h3>Total Trade Count</h3>
+                <p>7</p>
+            </div>
+            <div>
+                <h3>Overall Traded Volume</h3>
+                <p>8</p>
+            </div>
+
+
+        </div>
     </div>
   );
 }
