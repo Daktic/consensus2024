05ef0b36cd24f522e7acee6e9527af14cd15339e
Daktic <gberg@DakticDigital.com>
2024-05-30 14:59:10 -0500

    Added math skeleton

---
+++ frontend/src/scraper/math.js
@@ -0,0 +1,27 @@
+class AggMath {
+  static totalSupply(arr) {
+  //   TODO
+  }
+
+  static feeLock(arr) {
+    //   TODO
+  }
+  static reservedAmount(arr) {
+    //   TODO
+  }
+    static circulatingSupply(arr) {
+      //   TODO
+    }
+    static trustlessness(arr) {
+      //   TODO
+    }
+    static overallPaymentCount(arr) {
+      //   TODO
+    }
+    static totalPaymentsCount(arr) {
+      //   TODO
+    }
+    static totalTradeCount(arr) {
+      //   TODO
+    }
+}
